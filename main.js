d50cc0b99b7848a9622b67fe46d50238c09fb2a6
Edd Jhae <edd.bausa123@gmail.com>
2021-03-02 13:01:12 +0800

    added some animation and image

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 gsap.from('.anim1',
     {
         opacity: 0,
-        duration: 1.5,
-        stagger: 0.4,
+        duration: 1,
+        stagger: 0.5,
         y: -50
     })
+
+// gsap.to('.anim2',
+//     {
+//         scrollTrigger: {
+//             trigger: '.anim2',
+//             start: 'bottom center'
+//         },
+
+//         duration: 1,
+//         stagger: 0.5,
+
+//     })

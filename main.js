3ca082e8d59794fd87071f302c8b76ba4bfc9c59
Edd Jhae <edd.bausa123@gmail.com>
2021-03-24 11:14:48 +0800

    added scrolltrigger and footer

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-gsap.from('.anim1',
-    {
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.anim2',
+        start: "center bottom 100",
+    }
+});
+
+gsap.from('.anim1',{
         opacity: 0,
         duration: 1,
         stagger: 0.5,
-        y: -50
-    })
+        y: -50,
+});
 
-// gsap.to('.anim2',
-//     {
-//         scrollTrigger: {
-//             trigger: '.anim2',
-//             start: 'bottom center'
-//         },
+tl.from('.anim2', {
+    y: 200,
+    opacity: 0,
+    duration: 0.7,
+});
 
-//         duration: 1,
-//         stagger: 0.5,
-
-//     })
+tl.from('.title', {
+    opacity: 0,
+    duration: 0.3,
+});
